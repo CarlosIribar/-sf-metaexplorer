087df13a8a5b63db8482d51959dfe5ec5be896ef
Carlos Iribar <carlosiribar@gmail.com>
2026-02-09 11:51:39 -0300

    Initial commit: SF Metadata Explorer plugin scaffold

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+module.exports = {
+  extends: [
+    'eslint-config-salesforce-typescript',
+    'eslint-config-salesforce-license',
+    'plugin:sf-plugin/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['react', 'react-hooks'],
+  root: true,
+  settings: {
+    react: {
+      version: '18.3',
+    },
+  },
+};
